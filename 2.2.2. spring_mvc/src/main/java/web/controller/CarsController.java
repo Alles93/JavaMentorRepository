67665dbd75e16655377eeb4187b5962932ebb77b
Igor Pavlenkov <pavlenkov.igor@mail.ru>
2020-10-27 08:54:44 +0300

    2.2.2

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,21 +1,33 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class Cars {
-    @GetMapping(value = "cars")
-    public String printWelcome(ModelMap model) {
+public class CarsController {
+
+    CarService carService = new CarServiceImpl();
+
+    @GetMapping(value = "/cars")
+    public String printCars(@RequestParam(value = "count", required = false) Integer x, Model model) {
+        if (x == null) {
+            model.addAttribute("cars", carService.listCars(5));
+        } else {
+            model.addAttribute("cars", carService.listCars(x));
+        }
         List<String> messages = new ArrayList<>();
         messages.add("Hello! It's cars!");
         messages.add("Text for cars");
         messages.add("Cars");
         model.addAttribute("messages", messages);
-        return "index";
+        return "cars";
     }
 }
--- 2.2.2. spring_mvc/src/main/java/web/service/CarServiceImpl.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarServiceImpl.java
@@ -1,2 +1,20 @@
-package web.service;public class CarServiceImpl {
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public List<Car> listCars(int countCars) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Tesla",3));
+        cars.add(new Car("Audi",5));
+        cars.add(new Car("Nissan",2));
+        cars.add(new Car("Hummer",6));
+        cars.add(new Car("Pejo",10));
+
+        return countCars > 5 ? cars : cars.subList(0,countCars);
+    }
 }
